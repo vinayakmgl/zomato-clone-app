dfe0fd227990e416a21b3993fc7f215135637977
vinayak <vinayakmgl619@gmail.com>
2022-02-28 18:09:10 +0530

    filter part is done in delivery page

--- src/components/delivery/index.js
+++ src/components/delivery/index.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import Filters from '../common/filters'
 import "./delivery.css"
+
+const deliverFilters = [
+    {
+        id: 1,
+        icon: <i class="fi fi-rr-settings-sliders absolute-center"></i>,
+        title: "Filters",
+    },
+    {
+        id: 2,
+        title: "Rating: 4.0+",
+    },
+    {
+        id: 3,
+        title: "Safe and Hygienic",
+    },
+    {
+        id: 4,
+        title: "Pure veg",
+    },
+    {
+        id: 5,
+        title: "Delivery Time",
+        icon: <i className="fi fi-rr-apps-sort absolute-center"></i>,
+
+    },
+    {
+        id: 6,
+        title: "Great offer",
+    },
+]
 
 const Delivery = () => {
     return (
-        <div>this is delivery</div>
-    )
-}
+        <div>
+            <div className='max-width'>
+                <Filters filterlist={deliverFilters} />
+            </div>
+        </div>
+    );
+};
 
 export default Delivery
--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react'
-import Footer from '../../components/common/footer'
-import Header from '../../components/common/header'
-import Taboptions from '../../components/common/tabOptions'
-import Delivery from '../../components/delivery';
-import DiningOut from '../../components/diningOut';
-import Nightlife from '../../components/nightlife';
+import React, { useState } from "react";
+import Footer from "../../components/common/footer";
 
-const Homepage = () => {
-    const [activeTab, setActiveTab] = useState("Delivery")
+import Header from "../../components/common/header";
+import TabOptions from "../../components/common/tabOptions";
+import Delivery from "../../components/delivery";
+import DiningOut from "../../components/diningOut";
+import Nightlife from "../../components/nightlife";
+
+const HomePage = () => {
+    const [activeTab, setActiveTab] = useState("Delivery");
     return (
         <div>
             <Header />
-            <Taboptions activeTab={activeTab} setActiveTab={setActiveTab} />
+            <TabOptions activeTab={activeTab} setActiveTab={setActiveTab} />
             {getCorrectScreen(activeTab)}
             <Footer />
         </div>
@@ -21,15 +22,16 @@ const Homepage = () => {
 const getCorrectScreen = (tab) => {
     switch (tab) {
         case "Delivery":
-            return <Delivery />
-        case "Dining Out":
-            return <DiningOut />
-        case "Nightlife":
-            return <Nightlife />
-        default:
-            return <Delivery />
+            return <Delivery />;
 
+        case "Dining Out":
+            return <DiningOut />;
+
+        case "Nightlife":
+            return <Nightlife />;
+
+        default:
+            return <Delivery />;
     }
 };
-
-export default Homepage;
+export default HomePage;
